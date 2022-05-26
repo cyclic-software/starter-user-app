6b4e008245e88764795dd3d0ee77f2474c91ccb3
Kam Lasater <ckl@seekayel.com>
2022-05-26 15:56:51 -0400

    Linted auth

--- src/auth.js
+++ src/auth.js
@@ -1,22 +1,22 @@
 
 const { v4: uuid } = require('uuid')
-const crypto = require('crypto');
+const crypto = require('crypto')
 
-function securePassword(password) {
+function securePassword (password) {
   const salt = uuid()
-  return {hashed: hashWithSalt(password,salt), salt}
+  return { hashed: hashWithSalt(password, salt), salt }
 }
 
-function hashWithSalt(password,salt) {
-  const hash = crypto.createHmac('sha512', salt);
-  hash.update(password);
-  const hashed = hash.digest('hex');
+function hashWithSalt (password, salt) {
+  const hash = crypto.createHmac('sha512', salt)
+  hash.update(password)
+  const hashed = hash.digest('hex')
   return hashed
 }
 
-function testPassword(password,salt,hashed) {
-  console.log(password,salt,hashed)
-  return password && hashed === hashWithSalt(password,salt)
+function testPassword (password, salt, hashed) {
+  console.log(password, salt, hashed)
+  return password && hashed === hashWithSalt(password, salt)
 }
 
 module.exports = {
--- test/auth.test.js
+++ test/auth.test.js
@@ -1,24 +1,22 @@
 const auth = require('../src/auth.js')
 
-
-describe("auth.test.js", () => {
-  test("securePassword is different on different calls", async () => {
-    let pw1 = auth.securePassword('password')
-    let pw2 = auth.securePassword('password')
+describe('auth.test.js', () => {
+  test('securePassword is different on different calls', async () => {
+    const pw1 = auth.securePassword('password')
+    const pw2 = auth.securePassword('password')
     expect(pw1.hashed).not.toEqual(pw2.hashed)
     expect(pw1.salt).not.toEqual(pw2.salt)
-  });
+  })
 
   const passwordsToTry = [
     'password',
     '!@#$!%@^&$*(%^*$%$##!@%^&%%#$@#@:{L<>"A?DF>G<:FADLS{C:AWE">:<'
   ]
-  test("securePassword returns values that can be tested", async () => {
-
+  test('securePassword returns values that can be tested', async () => {
     passwordsToTry.map((pw) => {
-      let {salt,hashed} = auth.securePassword(pw)
-      expect(auth.hashWithSalt(pw,salt)).toEqual(hashed)
-      expect(auth.testPassword(pw,salt,hashed)).toEqual(true)
+      const { salt, hashed } = auth.securePassword(pw)
+      expect(auth.hashWithSalt(pw, salt)).toEqual(hashed)
+      expect(auth.testPassword(pw, salt, hashed)).toEqual(true)
     })
-  });
-});
+  })
+})
