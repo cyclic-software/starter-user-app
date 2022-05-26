26b70c08d70cdc3af95160907d939138790ac165
Kam Lasater <ckl@seekayel.com>
2022-05-26 13:09:03 -0400

    Added auth package

---
+++ src/auth.js
@@ -0,0 +1,26 @@
+
+const { v4: uuid } = require('uuid')
+const crypto = require('crypto');
+
+function securePassword(password) {
+  const salt = uuid()
+  return {hashed: hashWithSalt(password,salt), salt}
+}
+
+function hashWithSalt(password,salt) {
+  const hash = crypto.createHmac('sha512', salt);
+  hash.update(password);
+  const hashed = hash.digest('hex');
+  return hashed
+}
+
+function testPassword(password,salt,hashed) {
+  console.log(password,salt,hashed)
+  return password && hashed === hashWithSalt(password,salt)
+}
+
+module.exports = {
+  securePassword,
+  testPassword,
+  hashWithSalt
+}
